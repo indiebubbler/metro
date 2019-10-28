3073f028a5a2d89d0ec152f9fc2ae55359716bbe
unknown <krazybubbler@gmail.com>
2019-10-28 10:49:47 +0100

    Updates

---
+++ precache-manifest.fd566bb66c0be3dee33efaa2ff0c91c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe7699eaa2403fefd5f204b5fbfeb39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c508c2bbbdfd482e7556",
+    "url": "/static/css/2.95719dcc.chunk.css"
+  },
+  {
+    "revision": "e971533719234544c603",
+    "url": "/static/css/main.41134d7a.chunk.css"
+  },
+  {
+    "revision": "c508c2bbbdfd482e7556",
+    "url": "/static/js/2.6461986a.chunk.js"
+  },
+  {
+    "revision": "e971533719234544c603",
+    "url": "/static/js/main.b5f5436c.chunk.js"
+  },
+  {
+    "revision": "9135a1c78c2e2d722e0b",
+    "url": "/static/js/runtime-main.29d8e042.js"
+  }
+]);
